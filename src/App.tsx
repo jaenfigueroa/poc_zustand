78d1d2ee08cb369e6fe9fe13b7e2b1744755d8a3
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-09-12 18:59:52 -0500

    chore: add new methods

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,32 @@ import { useCounterStore } from './store/counterStore'
 // import shallow from 'zustand/shallow'
 
 const App = () => {
-  // const count = useCounterStore(({ count }) => count)
-
-  const counterStore = useCounterStore(
+  // 1RA FORMA
+  // EXTRAER ESPECÍFICAMENTE SOLO ESTAS PROPIEDADES, MEJOR OPTIMIZACION
+  // EVITAmos LA RENDERIZACIÓN INNECESARIA DE COMPONENTES SI OTRAS PARTES DEL ESTADO CAMBIAN
+  const { animals, persons } = useCounterStore(
     (state) => ({
-      count: state.count,
-      incrementCount: state.incrementCount,
-      decrementCount: state.decrementCount,
+      animals: state.animals,
+      persons: state.persons,
     })
     // shallow
   )
 
+  // 2DA FORMA
+  // EXTRAER TODAS LAS PROPIEDADES
+  const { incrementAnimals, decrementAnimals, multiplyPersons, dividePersons } = useCounterStore()
+
   return (
     <div className='app'>
-      <h2>Numero: {counterStore.count}</h2>
-      <button onClick={() => counterStore.incrementCount(2)}>Sumar 2</button>
-      <button onClick={() => counterStore.decrementCount(2)}>Restar 2</button>
+      <h2>Animales: {animals}</h2>
+      <h2>Personas: {persons}</h2>
+
+      <div className='buttonsContainer'>
+        <button onClick={() => incrementAnimals(2)}>Restar 2 animales</button>
+        <button onClick={() => decrementAnimals(2)}>Sumar 2 animales</button>
+        <button onClick={() => multiplyPersons(2)}>Multiplicar por 2 las personas</button>
+        <button onClick={() => dividePersons(2)}>Dividir entre 2 las personas</button>
+      </div>
     </div>
   )
 }
--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -1,21 +1,42 @@
 import { create } from 'zustand'
 
 interface CounterState {
-  count: number
-  incrementCount: (value: number) => void
-  decrementCount: (value: number) => void
+  animals: number
+  persons: number
+
+  incrementAnimals: (value: number) => void
+  decrementAnimals: (value: number) => void
+  multiplyPersons: (value: number) => void
+  dividePersons: (value: number) => void
 }
 
-export const useCounterStore = create<CounterState>((set) => ({
-  count: 10,
+export const useCounterStore = create<CounterState>((set, get) => ({
+  animals: 10,
+  persons: 50,
 
-  incrementCount: (value) =>
+  // 1RA FORMA
+  incrementAnimals: (value) =>
     set((state) => ({
-      count: state.count + value,
+      animals: state.animals + value,
     })),
 
-  decrementCount: (value) =>
+  decrementAnimals: (value) =>
     set((state) => ({
-      count: state.count - value,
+      animals: state.animals - value,
     })),
+
+  // 2DA FORMA, USANDO EL GET()
+  multiplyPersons: (value) => {
+    const { persons } = get()
+    set({
+      persons: persons * value,
+    })
+  },
+
+  dividePersons: (value) => {
+    const { persons } = get()
+    set({
+      persons: persons / value,
+    })
+  },
 }))
